8da406687940aece55b9ba6c5353be16345441a8
nehalb123 <nehal.borole@gmail.com>
2022-04-10 16:56:16 -0700

    updated

---
+++ uiuc/HSLAPixel.cpp
@@ -0,0 +1,30 @@
+/**
+ * @file HSLAPixel.cpp
+ * Implementation of the HSLAPixel class for use in with the PNG library.
+ *
+ * @author University of Illinois CS 225 Course Staff
+ * @version 2018r1-lab1 - Updated for CS 400
+ */
+
+#include <cmath>
+#include <iostream>
+#include "HSLAPixel.h"
+using namespace std;
+
+namespace uiuc {
+
+
+    HSLAPixel::HSLAPixel():HSLAPixel(0.0, 0.0, 0.0, 0.0){
+        
+    }
+
+    HSLAPixel::HSLAPixel(double h, double s, double l, double a){
+        this->h = h;
+        this->s = s;
+        this->l = l;
+        this->a = a;
+    }
+
+
+
+}
